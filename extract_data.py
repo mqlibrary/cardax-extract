8b722d0e0d7a1ed8e2ac995b0f5fec951859fa22
nishen <nishen.naidoo@mq.edu.au>
2019-10-24 15:07:49 +1100

    Changed the door selection to a configuration option.

--- extract_data.py
+++ extract_data.py
@@ -116,16 +116,14 @@ def extract_events(pos=None):
     cardaxdb_dao = CardaxDbDAO(create_engine(config.cardaxdb_conn))
     cardaxdb_dao.initialise_schema_events()
 
-    door_list = ["48970", "48972", "48974", "48982", "48998", ]
-
     max_pos = elastic_dao.get_max_pos() if pos is None else pos
     log.info("fetching events from: %s", max_pos)
-    events, pos = cardax_dao.fetch_events(group=23, doors=",".join(door_list), pos=max_pos, top=2000)
+    events, pos = cardax_dao.fetch_events(group=23, doors=",".join(config.cardax_doors), pos=max_pos, top=2000)
     while len(events) > 0:
         log.info("saving events[%s]: %s", pos, len(events))
         elastic_dao.save_events(events)
         log.info("fetching events from: %s", pos)
-        events, pos = cardax_dao.fetch_events(group=23, doors=",".join(door_list), pos=pos, top=2000)
+        events, pos = cardax_dao.fetch_events(group=23, doors=",".join(config.cardax_doors), pos=pos, top=2000)
 
     log.info("extraction complete")
 
